79826540754ddaa389cec2d981375abe32d5432a
github <pragya.sharma2_cs19@gla.ac.in>
2021-05-05 13:29:42 +0530

    write and read of a file

--- File System/script.js
+++ File System/script.js
@@ -1,3 +1,10 @@
 const fs = require('fs')
 // this is the way to import library in javascript.
  //fs -- file system
+
+ fs.writeFileSync('helloword.txt', "hello World")
+ //can also write fs.writeFile('filename', txt)
+ const data = fs.readFileSync('helloword.txt')
+ 
+ console.log(data)
+ console.log(data.toString())
