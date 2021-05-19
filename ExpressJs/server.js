21327402d91cff86348d49b8a679bdcebf727c15
github <pragya.sharma2_cs19@gla.ac.in>
2021-05-19 14:57:15 +0530

    make a  server file and code ...

---
+++ ExpressJs/server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+app.get('/',(req, res)=>{
+    res.send("Hello World")
+})
+app.listen(4444, ()=>{
+    console.log('server started on hhtp://localhost:444')
+})
