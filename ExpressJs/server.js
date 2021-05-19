8bcdc7f214a6baa88b16f1ee4256e0af452c9ac4
github <pragya.sharma2_cs19@gla.ac.in>
2021-05-19 15:08:43 +0530

    commit to add some comments and more code to show changes

--- ExpressJs/server.js
+++ ExpressJs/server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 app.get('/',(req, res)=>{
-    res.send("Hello World")
+    res.send('<h1 style="color:red;">Hello World</h1>')//response
+    
 })
 app.listen(4444, ()=>{
-    console.log('server started on hhtp://localhost:444')
+    console.log('server started on http://localhost:4444') //4444 is port.
+    //give port greater than 1024 coz 0 to 1024 are reserved ports.
 })
