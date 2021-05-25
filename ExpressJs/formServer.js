0865be2ae9db96212658cb0e9df3595fbdef57bc
github <pragya.sharma2_cs19@gla.ac.in>
2021-05-25 16:50:19 +0530

    get query from client

--- ExpressJs/formServer.js
+++ ExpressJs/formServer.js
@@ -3,3 +3,11 @@ let formApp=express()
 formApp.listen('4444',()=>{
     console.log("server started on localhost:4444")
 })
+
+formApp.get('/holi',(req,res)=>{
+    let name = "The Anonymous You"
+    if(req.query.name){
+        name=req.query.name
+    }
+    res.send("Happy Holi "+name)
+})
