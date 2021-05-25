f3eb37ce040a86a77cb77a18a41ce40318e815fc
github <pragya.sharma2_cs19@gla.ac.in>
2021-05-25 16:00:09 +0530

    wish happy holi to person

--- ExpressJs/server.js
+++ ExpressJs/server.js
@@ -12,6 +12,13 @@ app.get('/greet', (req,res)=>{
     console.log(req)
     res.send('<h1>Good Morning</h1>')
 })
+app.get('/holi', (req,res)=>{
+    let person='Guest';
+    if(req.query.person){
+        person=req.query.person
+    }
+    res.send("Happy Holi", person)
+})
 app.listen(4444, ()=>{
     console.log('server started on http://localhost:4444') //4444 is port.
     //give port greater than 1024 coz 0 to 1024 are reserved ports.
