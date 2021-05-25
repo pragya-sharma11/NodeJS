eda9d55229314dfed4413bcc42416352e3062343
github <pragya.sharma2_cs19@gla.ac.in>
2021-05-25 15:43:43 +0530

    make a greet

--- ExpressJs/server.js
+++ ExpressJs/server.js
@@ -5,6 +5,9 @@ app.get('/',(req, res)=>{
     res.send("Hello I am pragya!!!")  
     //can send plain text or html code.
 })
+app.get('/greet', (req,res)=>{
+    res.send('<h1>Good Morning</h1>')
+})
 app.listen(4444, ()=>{
     console.log('server started on http://localhost:4444') //4444 is port.
     //give port greater than 1024 coz 0 to 1024 are reserved ports.
