b82e4621dd40a7243823372055dbf6ea1983cd71
github <pragya.sharma2_cs19@gla.ac.in>
2021-06-17 15:48:23 +0530

    send html body to home page using nodejs

--- ExpressJs/ToDoListServer.js
+++ ExpressJs/ToDoListServer.js
@@ -1,10 +1,19 @@
-const express = import('express')
+const express = require('express')
 const app = express()
 app.listen(5555,()=>{
     console.log("Server is running on port 5555.")
 })
 
 app.get('/',(req,res)=>{
-    res.send()
+    res.send(`
+    <html>
+    <body>
+    <form>
+    <input name='task'>
+    <button type='submit'>Add</button>
+    </form>
+    </body> 
+    </html>
+    `)
 })
 
