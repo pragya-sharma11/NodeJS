d53409869f03512690b561e36416cced93e9ae12
github <pragya.sharma2_cs19@gla.ac.in>
2021-06-17 13:50:22 +0530

    add method use.

--- ExpressJs/formServer.js
+++ ExpressJs/formServer.js
@@ -5,7 +5,8 @@ formApp.listen('4444',()=>{
 })
 formApp.use(express.urlencoded({extended : true}))
 //this above function runs before the get , post push etc methods.
-
+//it parses the file, data which comes to server as urlencoded format.
+//if the above method is not used then it will not encode the name value in post and give error.
 formApp.get('/holi',(req,res)=>{
     let name = "The Anonymous You"
     if(req.query.name){
@@ -15,14 +16,15 @@ formApp.get('/holi',(req,res)=>{
 })
 formApp.post('/holi',(req,res)=>{
     let name = "The Anonymous You"
-    if(req.query.name){
-        name=req.query.name
+    if(req.body.name){
+        name=req.body.name
     }
     res.send("Happy Holi "+name)
 })
 /**
  * by post method, we can't give any name .
  * As u see it does not accept the value in query of request.
+ * It post the value in body of request.
  */
 formApp.get('/form',(req,res)=>{
     res.sendFile(__dirname + '/files/WishingForm.html')
