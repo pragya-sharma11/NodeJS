899a7c4c4deab14ee72f521aa83ff40320e86b49
github <pragya.sharma2_cs19@gla.ac.in>
2021-06-17 13:39:17 +0530

    put method in jsServer

--- ExpressJs/formServer.js
+++ ExpressJs/formServer.js
@@ -11,7 +11,17 @@ formApp.get('/holi',(req,res)=>{
     }
     res.send("Happy Holi "+name)
 })
-
+formApp.post('/holi',(req,res)=>{
+    let name = "The Anonymous You"
+    if(req.query.name){
+        name=req.query.name
+    }
+    res.send("Happy Holi "+name)
+})
+/**
+ * by post method, we can't give any name .
+ * As u see it does not accept the value in query of request.
+ */
 formApp.get('/form',(req,res)=>{
     res.sendFile(__dirname + '/files/WishingForm.html')
 })
