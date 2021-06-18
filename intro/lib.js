5d2620eda9969c641fa400b3e185a06ae92127cc
github <pragya.sharma2_cs19@gla.ac.in>
2021-06-18 17:53:15 +0530

    commit

--- intro/lib.js
+++ intro/lib.js
@@ -1,8 +1,12 @@
 function awesomeFunction(name){
     console.log("Hey!! "+name+". This is awesome!!!")
 }
-console.log(module.exports) //
+//console.log(module.exports) //
 module.exports={ //to make use of this function out of script
     awesomeFunction
 }
-console.log(module.exports)
+//console.log(module.exports)
+setTimeout(()=>{
+    module.exports.omg='wow';
+},2000)
+console.log(module.exports)//awesomeFunction only
--- intro/main.js
+++ intro/main.js
@@ -6,4 +6,11 @@ else{
     console.log('running in node js.')
 }
 const lib=require('./lib')
+console.log(lib)  //only awesomeFunction will be in the lib object
 lib.awesomeFunction("Mr.Bean")
+
+setTimeout(()=>{
+    const lib_new = require('./lib')
+    console.log(lib_new) // now the lib will be update and has omg object too.
+},2000)
+console.log(lib)
