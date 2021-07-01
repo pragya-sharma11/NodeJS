3c677e776e628a79be25fa7fa0aed3ac8db0c6b1
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-02 00:18:26 +0530

    create middlewares

--- ExpressJs/middlewares/server.js
+++ ExpressJs/middlewares/server.js
@@ -4,5 +4,25 @@ app.listen(5454,()=>{
     console.log("server is at port 5454")
 })
 app.get('/',(req,res)=>{
-    res.send("Hello BRO!!")
+    res.send("Hello World!!")
 })
+function m1(req,res,next){
+    console.log("middleware 1")
+    next()
+}
+function m2(req,res,next){
+    console.log("middleware 1")
+    next()
+}
+function m3(req,res,next){
+    console.log("middleware 1")
+    next()
+}
+function m4(req,res,next){
+    console.log("middleware 1")
+    next()
+}
+function m5(req,res,next){
+    console.log("middleware 1")
+    next()
+}
