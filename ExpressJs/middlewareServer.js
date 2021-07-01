6d6e4748c81edcd460924e529700136c0ca00483
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-01 22:53:16 +0530

    write comments for better understanding

--- ExpressJs/middlewareServer.js
+++ ExpressJs/middlewareServer.js
@@ -11,6 +11,8 @@ app.listen(4321,()=>{
 app.use('/xyz',express.static(__dirname +'/public'))
 /**
  * here we are mounting public folder on xyz path.
+ * if i write localhost:4321/xyz--->nothing will be found coz it a path on which folder is mounted.
+ * if public folder has a file named index.html(standard notation) then localhost4321/xyz will directed to index.html file.
  * public folder will have files which are used as UI or acessible to user.
  * this above function has same signature as get().
  * localhost:4321/xyz/file.html-->for user shown fime direction.
