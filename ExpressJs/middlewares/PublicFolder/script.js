62ed7c08f1b4032730d6f87174aa2c538f4400bb
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-03 14:28:45 +0530

    commit

---
+++ ExpressJs/middlewares/PublicFolder/script.js
@@ -0,0 +1,8 @@
+let inpCode = document.getElementById('inpCode')
+let btnEncode = document.getElementById('btnEncode')
+
+let code = document.getElementById('code')
+
+btnEncode.onclick = function(){
+    let data = 
+}
