a99e282702cd0aa72843d820c8db60c6e9857ef0
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-05 22:38:35 +0530

    commit

--- ExpressJs/NewFolder/routers/todos.js
+++ ExpressJs/NewFolder/routers/todos.js
@@ -1,9 +1,12 @@
 const route = require('express')
 let todos = [];
 route.length('/',(req,res)=>{
-
+    res.send(todos)
 })
 route.post('/',(req,res)=>{
-
+    todos.push({
+        task:req.body.task
+    })
+    res.send(todos)
 })
 module.exports = route
