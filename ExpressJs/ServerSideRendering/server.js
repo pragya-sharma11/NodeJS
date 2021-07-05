828bf7f6ea04763b6d1ceaae6b8633914800263a
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-06 00:51:46 +0530

    commit

--- ExpressJs/ServerSideRendering/server.js
+++ ExpressJs/ServerSideRendering/server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const srv = express()
-const todoRoute = require('/todos') 
+const todoRoute = require('./todo') 
 srv.listen(4444)
 srv.use(express.json())
 srv.use(express.urlencoded({extended:true}))
 srv.set('view engine','hbs')
-srv.set('views','views') //second parameter is name of our folder in which view files are located.
+srv.set('views',__dirname+ '/views') //second parameter is name of our folder in which view files are located.
+
 srv.use('/todos',todoRoute)
