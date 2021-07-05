39ff21a65eb0dece0318f9363692c397ae2e551e
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-06 00:36:32 +0530

    commit

--- ExpressJs/ServerSideRendering/server.js
+++ ExpressJs/ServerSideRendering/server.js
@@ -4,5 +4,6 @@ const todoRoute = require('/todos')
 srv.listen(4444)
 srv.use(express.json())
 srv.use(express.urlencoded({extended:true}))
-srv,set('view engine','hbs')
+srv.set('view engine','hbs')
+srv.set('views','views') //second parameter is name of our folder in which view files are located.
 srv.use('/todos',todoRoute)
--- ExpressJs/ServerSideRendering/todo.js
+++ ExpressJs/ServerSideRendering/todo.js
@@ -1,6 +1,14 @@
 const route =require('express').Router()
+let todos = [
+    {task : "First task"},
+    {task : 'Second task'}
+]
 route.get('/',function(req,res){
-
+    res.render('todos',{todos})
+    //second argument is object.:-todos is an array object.
+    //we willl not write todos.hbs coz we have already told server that 
+    //we are using view engine of hbs. 
+    //when we put todos it will go to todos.hbs and is todos object is an array it wll insert the elements in ul.
 })
 route.post('/',(req,res)=>{
 
