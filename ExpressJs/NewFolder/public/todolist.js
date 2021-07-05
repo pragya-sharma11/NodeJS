f456126b24c1ae98c0e9dfd7b46ba8b518fc2653
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-05 23:46:02 +0530

    commit

--- ExpressJs/NewFolder/public/todolist.js
+++ ExpressJs/NewFolder/public/todolist.js
@@ -6,8 +6,24 @@ $(function(){
     addtodobtn.click(()=>{
         console.log("Button is clicked!!.")
         let newTodo = newTodoBox.val()
-        $.post()//it does ajax(Asysnchronous javascript and xml)
-        //aysnch:-without reloading your page, our page will still post 
+        //post is used to make a new post request.
+        $.post(
+            '/todos/',
+            {task : newTodo}, 
+            function(data){
+                console.log(data)//array of todolist
+                todoList.empty();
+                for(todo of data){
+                    todoList.append("<li>"+todo.task+"</li>")
+                    
+                }
+            }
+        )
+        //when u make a post req, our page makes a post request to server and response comes back to page but page is not refresh only some data is downloaded to page.
+        //it does ajax(Asysnchronous javascript and xml)
+        //aysnch:-without reloading your page or without making a new request in url bar, our page will still make some get and post from browser(client pc) to server
+
+
     })
 
 })
