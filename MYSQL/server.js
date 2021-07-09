a0661ec3b64765b2b4fdd1312fc0668b1c0e68db
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-09 14:49:21 +0530

    render the added data

--- MYSQL/server.js
+++ MYSQL/server.js
@@ -7,3 +7,14 @@ app.set("view engine", "hbs")
 app.listen(4444,()=>{
     console.log("server started on http://localhost:4444")
 })
+app.get('/', (req,res)=>{
+    res.render('persons',{
+        persons:[
+            {name:'abc', age:22, city:'abcCity'},
+            {name:'abc', age:22, city:'abcCity'}
+        ]
+    })
+})
+app.get('/add', (res, req)=>{
+    res.render('person_add')
+})
