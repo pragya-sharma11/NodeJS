e108f48003b9cd7716a68fea234d4abea6ef0fc1
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-09 15:45:50 +0530

    create pages.js and call db here

--- MYSQL/server.js
+++ MYSQL/server.js
@@ -8,33 +8,3 @@ app.set('views',__dirname+ '/views')
 app.listen(4444,()=>{
     console.log("server started on http://localhost:4444")
 })
-app.get('/', (req,res)=>{
-/*    res.render('person',{
-        person:[
-            {name:'abc', age:22, city:'abcCity'},
-            {name:'abc', age:22, city:'abcCity'}
-        ]
-    })
-*/
-    db.getAllUsers()
-    .then((person)=>{
-        res.render('person', {person})
-    })
-    .catch((err=>{
-        res.send(err)
-    }))
-
-})
-
-app.get('/add', (req, res)=>{
-    res.render('person_add')
-})
-app.post('/add', (req, res)=>{
-    db.addUsers(req.body.name, req.body.age, req.body.city)
-    .then(()=>{
-        res.redirect('/')
-    })
-    .catch((err)=>{
-        res.send(err)
-    })
-})
