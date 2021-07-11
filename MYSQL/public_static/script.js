0c523386ba9b725785e616745d3ff42b896a374f
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-11 14:35:25 +0530

    commit for starting of api

--- MYSQL/public_static/script.js
+++ MYSQL/public_static/script.js
@@ -4,4 +4,10 @@ $(()=>{
     let inp_city = $('$city')
     let btn_submit = $('$submit')
     let tbl_persons = $('#persons')
+    btn_submit.click(()=>{
+        console.log('btn clicked')
+        //create api to make functioning of submit button
+        //this api basically connect front end with backend
+        
+    })
 })
