5427a8d5f2adf82814324130831982d23b184276
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-11 14:41:27 +0530

    make get and post methods for api

--- MYSQL/routes/api.js
+++ MYSQL/routes/api.js
@@ -1,4 +1,18 @@
 const route = require('express').route
+//This route is on "/api/ path"
+const db = require("../db")
+route.get('/',(req,res)=>{
+    //send all persons as array here
+    db.getAllUsers()
+    .then((person)=> res.send(person))
+    .catch((err)=>res.send(err))
+})
+
+route.post('/',(req,res)=>{
+    db.addUsers(req.body.name,req.body.age,req.body.city)
+    .then(()=>res.redirect("/api/"))
+    .catch((err)=>res.send(err))
+})
 exports = module.exports={
     route
 }
