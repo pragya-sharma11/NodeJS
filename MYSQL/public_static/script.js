a747013e8dcc4da2f55e3bdece32b3ec2fd7a7a6
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-11 14:46:06 +0530

    refresh table functionality

--- MYSQL/public_static/script.js
+++ MYSQL/public_static/script.js
@@ -4,10 +4,30 @@ $(()=>{
     let inp_city = $('$city')
     let btn_submit = $('$submit')
     let tbl_persons = $('#persons')
+    function refresh(persons) {
+        tbl_persons.empty()
+        tbl_persons.append(`
+            <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>City</th>
+        </tr>`
+        )
+        for(person of persons){
+            tbl_persons.append(
+                `<tr>
+                <td>${persons.name}</td>
+                <td>${persons.age}</td>
+                <td>${persons.city}</td>
+            </tr>`
+            )
+        }
+    }
+    $.get("/api/")
     btn_submit.click(()=>{
         console.log('btn clicked')
         //create api to make functioning of submit button
         //this api basically connect front end with backend
-        
+
     })
 })
--- MYSQL/routes/api.js
+++ MYSQL/routes/api.js
@@ -1,14 +1,15 @@
 const route = require('express').route
 //This route is on "/api/ path"
 const db = require("../db")
-route.get('/',(req,res)=>{
+route.get('/persons/',(req,res)=>{
     //send all persons as array here
     db.getAllUsers()
     .then((person)=> res.send(person))
     .catch((err)=>res.send(err))
 })
 
-route.post('/',(req,res)=>{
+route.post('/persons/',(req,res)=>{
+    //add persons details
     db.addUsers(req.body.name,req.body.age,req.body.city)
     .then(()=>res.redirect("/api/"))
     .catch((err)=>res.send(err))
