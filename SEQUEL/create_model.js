b86ab0e287a6d706d83baafd38d5b8111e404b5e
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-12 13:30:12 +0530

    insertion way 2

--- SEQUEL/create_model.js
+++ SEQUEL/create_model.js
@@ -21,10 +21,17 @@ const task = async ()=>{
     try{
         await db.sync()
         //insert the row in database
-        await student.create({
+        /*await student.create({
             name:'some name',
             age:22
-        })
+        })*/
+        for(let i=1;i<=30;i++){
+            await student.create({
+                name :(['Qian', 'zheng', 'linye', 'moyon', 'junki', 'xiao', 
+                'gangzi', 'munyon', 'anxin', 'lyue'])[parseInt(Math.random()*10)],
+                age:(parseInt(Math.random()*10))+10
+            })
+        }
     }
     catch (err) {
         console.log(err)
