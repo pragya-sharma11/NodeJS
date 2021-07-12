cb6054c7e18c47247bdf3eeafb1fc7b3d0a4c64c
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-12 12:59:39 +0530

    add comments

--- SEQUEL/connect.js
+++ SEQUEL/connect.js
@@ -2,8 +2,12 @@ const sequilize = require('sequelize')
 
 const db = new sequelize('sampledb1', 'sampleuser1', 'samplepassword',{
     host:'localhost'
-    dialect:'mysql'
+    dialect:'mysql' //this attribute basically tell which type of database we are using, According to which it relates the queries.
 })
 db.authenticate()
 .then(()=>console.log("connection worked"))
 .catch((err)=>console.log(err))
+
+/**
+ * sequelize is used to make models to for relational databases.
+ */
