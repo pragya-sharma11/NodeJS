43f354b54cc3a69b7e8d34cd9bc312af8bb05e33
github <pragya.sharma2_cs19@gla.ac.in>
2021-07-12 13:07:47 +0530

    create a table

--- SEQUEL/create_model.js
+++ SEQUEL/create_model.js
@@ -1,8 +1,19 @@
 const sequelize = require('sequelize')
-const DATATYPES = sequelize.DATATYPES
+const DATATYPES = sequelize.DATATYPES //defines the datatypes 
 
 const db = new sequelize('sampledb1', 'sampleuser1', 'samplepassword',{
     host:'localhost'
     dialect:'mysql' 
 })
 
+const student = db.define('Student',{ //create a table in db named Student and inside this object in next param, we have given column names and thier properties. 
+    name:{
+        type:DATATYPES.STRING(15),
+        allowNull:false    
+    },
+    age:{
+        type:DATATYPES.INTEGER,
+        allowNull:true,
+        default:0
+    }
+})
